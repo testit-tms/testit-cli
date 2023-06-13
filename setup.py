a1c275ffa8c459436e5975efcb2f4dac432afab3
Tatiana9885 <113346777+Tatiana9885@users.noreply.github.com>
2023-06-13 12:50:53 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='testit-cli',
-    version='0.1.8',
+    version='0.1.9',
     description='This tool is the command line wrapper of Test IT allowing you to upload the test results in real time '
                 'to Test IT',
     long_description=open('README.md', "r").read(),
