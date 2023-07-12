a83232739d737d488836706c8b861da2144d818d
pavel.butuzov <pavel.butuzov@testit.software>
2023-07-12 16:31:07 +0300

    Added changes from code review.

--- src/testit_cli/click_commands.py
+++ src/testit_cli/click_commands.py
@@ -17,17 +17,15 @@ def results():
 @results.command("upload")
 @click.option("-u", "--url", type=str, prompt="Set url", help="Set url address of the Test IT instance (https://demo.testit.software)")
 @click.option("-t", "--token", type=str, prompt="Set API token", help="Set API token (T2lKd2pLZGI4WHRhaVZUejNl)")
-@click.option("-pi", "--project-id", type=str, prompt="Set project id", help="Set project id (5236eb3f-7c05-46f9-a609-dc0278896464)")
 @click.option("-ci", "--configuration-id", type=str, prompt="Set configuration id", help="Set configuration id (15dbb164-c1aa-4cbf-830c-8c01ae14f4fb)")
-@click.option("-ti", "--testrun-id", type=str, help="Set test run id (3802f329-190c-4617-8bb0-2c3696abeb8f)", default=None)
-@click.option("-tn", "--testrun-name", type=str, help="Set test run name (TestRun01)", default=None)
+@click.option("-ti", "--testrun-id", type=str, prompt="Set test run id", help="Set test run id (3802f329-190c-4617-8bb0-2c3696abeb8f)")
 @click.option("-s", "--separator", type=str, help="Separate the classname value in the results into namespace and classname (.)", default=None)
 @click.option("-ns", "--namespace", type=str, help="Set namespace (NameSpace01)", default=None)
 @click.option("-cn", "--classname", type=str, help="Set classname (ClassName01)", default=None)
 @click.option("-r", "--results", type=str, prompt="Set directory with results", help="Set directory with results file (DIR)")
 @click.option("-d", "--debug", is_flag=True, help="Set debug logs")
-def upload_results(url, token, project_id, configuration_id, testrun_id, testrun_name, separator, namespace, classname, results, debug):
-    config = Config(url, token, project_id, configuration_id, testrun_id, testrun_name, separator, namespace, classname, results, debug, "")
+def upload_results(url, token, configuration_id, testrun_id, separator, namespace, classname, results, debug):
+    config = Config(url, token, "", configuration_id, testrun_id, "", separator, namespace, classname, results, debug, "")
     service = ServiceFactory().get(config)
 
     service.upload_results()
@@ -64,13 +62,7 @@ def testrun():
 @click.option("-tn", "--testrun-name", type=str, help="Set test run name (TestRun01)", default=None)
 @click.option("-o", "--output", type=str, prompt="Set file path", help="Set file path for output (FILE)")
 @click.option("-d", "--debug", is_flag=True, help="Set debug logs")
-def create_testrun(
-        url,
-        token,
-        project_id,
-        testrun_name,
-        output,
-        debug):
+def create_testrun(url, token, project_id, testrun_name, output, debug):
     config = Config(url, token, project_id, "", "", testrun_name, "", "", "", "", debug, output)
     service = ServiceFactory().get(config)
 
