096b23ac0a347bb5146bf257b9e7b7b35a54eae0
pavel.butuzov <pavel.butuzov@testit.software>
2023-07-20 16:09:35 +0300

    Added validation for required parameters.

--- src/testit_cli/validation.py
+++ src/testit_cli/validation.py
@@ -1,17 +1,10 @@
 import click
 import validators
 
-from uuid import UUID
-
 
 def validate_uuid(ctx, param, value):
     if value is not None:
-        try:
-            uuid_obj = UUID(value, version=4)
-
-            if not str(uuid_obj) == value:
-                raise
-        except:
+        if not validators.uuid(value):
             raise click.BadParameter(f'"{value}" uuid of {param.name} (3802f329-190c-4617-8bb0-2c3696abeb8f)')
 
     return value
