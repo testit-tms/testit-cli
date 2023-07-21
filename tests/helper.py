02ac97b5c918f09d67d5b85e987f85cdffa51725
pavel.butuzov <pavel.butuzov@testit.software>
2023-07-21 16:32:31 +0300

    Added tests in workflow.

--- tests/helper.py
+++ tests/helper.py
@@ -10,36 +10,210 @@ class Helper:
     __FILE_PATH = "FILE"
 
     @classmethod
-    def get_input_for_results_import_without_arguments(cls):
-        return f"{cls.__URL}\n{cls.__TOKEN}\n{cls.__UUID}\n{cls.__UUID}\n{cls.__DIR_PATH}\n"
+    def get_command_results_import_with_long_arguments_without_url_argument(cls):
+        return ["results", "import", "--token", cls.__TOKEN, "--project-id", cls.__UUID, "--configuration-id", cls.__UUID, "--results", cls.__DIR_PATH]
 
     @classmethod
-    def get_output_for_results_import_without_arguments(cls):
-        return f"Set url: {cls.__URL}\nSet API token: {cls.__TOKEN}\nSet project id: {cls.__UUID}\nSet configuration id: {cls.__UUID}\nSet directory with results: {cls.__DIR_PATH}\n"
+    def get_command_results_import_with_short_arguments_without_url_argument(cls):
+        return ["results", "import", "-t", cls.__TOKEN, "-pi", cls.__UUID, "-ci", cls.__UUID, "-r", cls.__DIR_PATH]
 
     @classmethod
-    def get_input_for_results_upload_without_arguments(cls):
-        return f"{cls.__URL}\n{cls.__TOKEN}\n{cls.__UUID}\n{cls.__UUID}\n{cls.__DIR_PATH}\n"
+    def get_output_for_results_import_without_url_argument(cls):
+        return f"results import --help' for help.\n\nError: Missing option '-u' / '--url'.\n"
 
     @classmethod
-    def get_output_for_results_upload_without_arguments(cls):
-        return f"Set url: {cls.__URL}\nSet API token: {cls.__TOKEN}\nSet configuration id: {cls.__UUID}\nSet test run id: {cls.__UUID}\nSet directory with results: {cls.__DIR_PATH}\n"
+    def get_command_results_import_with_long_arguments_without_token_argument(cls):
+        return ["results", "import", "--url", cls.__URL, "--project-id", cls.__UUID, "--configuration-id", cls.__UUID, "--results", cls.__DIR_PATH]
 
     @classmethod
-    def get_input_for_testrun_create_without_arguments(cls):
-        return f"{cls.__URL}\n{cls.__TOKEN}\n{cls.__UUID}\n{cls.__FILE_PATH}\n"
+    def get_command_results_import_with_short_arguments_without_token_argument(cls):
+        return ["results", "import", "-u", cls.__URL, "-pi", cls.__UUID, "-ci", cls.__UUID, "-r", cls.__DIR_PATH]
 
     @classmethod
-    def get_output_for_testrun_create_without_arguments(cls):
-        return f"Set url: {cls.__URL}\nSet API token: {cls.__TOKEN}\nSet project id: {cls.__UUID}\nSet file path: {cls.__FILE_PATH}\n"
+    def get_output_for_results_import_without_token_argument(cls):
+        return f"results import --help' for help.\n\nError: Missing option '-t' / '--token'.\n"
 
     @classmethod
-    def get_input_for_testrun_complete_without_arguments(cls):
-        return f"{cls.__URL}\n{cls.__TOKEN}\n{cls.__UUID}\n"
+    def get_command_results_import_with_long_arguments_without_project_id_argument(cls):
+        return ["results", "import", "--url", cls.__URL, "--token", cls.__TOKEN, "--configuration-id", cls.__UUID, "--results", cls.__DIR_PATH]
 
     @classmethod
-    def get_output_for_testrun_complete_without_arguments(cls):
-        return f"Set url: {cls.__URL}\nSet API token: {cls.__TOKEN}\nSet test run id: {cls.__UUID}\n"
+    def get_command_results_import_with_short_arguments_without_project_id_argument(cls):
+        return ["results", "import", "-u", cls.__URL, "-t", cls.__TOKEN, "-ci", cls.__UUID, "-r", cls.__DIR_PATH]
+
+    @classmethod
+    def get_output_for_results_import_without_project_id_argument(cls):
+        return f"results import --help' for help.\n\nError: Missing option '-pi' / '--project-id'.\n"
+
+    @classmethod
+    def get_command_results_import_with_long_arguments_without_configuration_id_argument(cls):
+        return ["results", "import", "--url", cls.__URL, "--token", cls.__TOKEN, "--project-id", cls.__UUID, "--results", cls.__DIR_PATH]
+
+    @classmethod
+    def get_command_results_import_with_short_arguments_without_configuration_id_argument(cls):
+        return ["results", "import", "-u", cls.__URL, "-t", cls.__TOKEN, "-pi", cls.__UUID, "-r", cls.__DIR_PATH]
+
+    @classmethod
+    def get_output_for_results_import_without_configuration_id_argument(cls):
+        return f"results import --help' for help.\n\nError: Missing option '-ci' / '--configuration-id'.\n"
+
+    @classmethod
+    def get_command_results_import_with_long_arguments_without_results_argument(cls):
+        return ["results", "import", "--url", cls.__URL, "--token", cls.__TOKEN, "--project-id", cls.__UUID, "--configuration-id", cls.__UUID]
+
+    @classmethod
+    def get_command_results_import_with_short_arguments_without_results_argument(cls):
+        return ["results", "import", "-u", cls.__URL, "-t", cls.__TOKEN, "-pi", cls.__UUID, "-ci", cls.__UUID]
+
+    @classmethod
+    def get_output_for_results_import_without_results_argument(cls):
+        return f"results import --help' for help.\n\nError: Missing option '-r' / '--results'.\n"
+
+    @classmethod
+    def get_command_results_upload_with_long_arguments_without_url_argument(cls):
+        return ["results", "upload", "--token", cls.__TOKEN, "--testrun-id", cls.__UUID, "--configuration-id", cls.__UUID, "--results", cls.__DIR_PATH]
+
+    @classmethod
+    def get_command_results_upload_with_short_arguments_without_url_argument(cls):
+        return ["results", "upload", "-t", cls.__TOKEN, "-ti", cls.__UUID, "-ci", cls.__UUID, "-r", cls.__DIR_PATH]
+
+    @classmethod
+    def get_output_for_results_upload_without_url_argument(cls):
+        return f"results upload --help' for help.\n\nError: Missing option '-u' / '--url'.\n"
+
+    @classmethod
+    def get_command_results_upload_with_long_arguments_without_token_argument(cls):
+        return ["results", "upload", "--url", cls.__URL, "--testrun-id", cls.__UUID, "--configuration-id", cls.__UUID,
+                "--results", cls.__DIR_PATH]
+
+    @classmethod
+    def get_command_results_upload_with_short_arguments_without_token_argument(cls):
+        return ["results", "upload", "-u", cls.__URL, "-ti", cls.__UUID, "-ci", cls.__UUID, "-r", cls.__DIR_PATH]
+
+    @classmethod
+    def get_output_for_results_upload_without_token_argument(cls):
+        return f"results upload --help' for help.\n\nError: Missing option '-t' / '--token'.\n"
+
+    @classmethod
+    def get_command_results_upload_with_long_arguments_without_testrun_id_argument(cls):
+        return ["results", "upload", "--url", cls.__URL, "--token", cls.__TOKEN, "--configuration-id", cls.__UUID,
+                "--results", cls.__DIR_PATH]
+
+    @classmethod
+    def get_command_results_upload_with_short_arguments_without_testrun_id_argument(cls):
+        return ["results", "upload", "-u", cls.__URL, "-t", cls.__TOKEN, "-ci", cls.__UUID, "-r", cls.__DIR_PATH]
+
+    @classmethod
+    def get_output_for_results_upload_without_testrun_id_argument(cls):
+        return f"results upload --help' for help.\n\nError: Missing option '-ti' / '--testrun-id'.\n"
+
+    @classmethod
+    def get_command_results_upload_with_long_arguments_without_configuration_id_argument(cls):
+        return ["results", "upload", "--url", cls.__URL, "--token", cls.__TOKEN, "--testrun-id", cls.__UUID, "--results", cls.__DIR_PATH]
+
+    @classmethod
+    def get_command_results_upload_with_short_arguments_without_configuration_id_argument(cls):
+        return ["results", "upload", "-u", cls.__URL, "-t", cls.__TOKEN, "-ti", cls.__UUID, "-r", cls.__DIR_PATH]
+
+    @classmethod
+    def get_output_for_results_upload_without_configuration_id_argument(cls):
+        return f"results upload --help' for help.\n\nError: Missing option '-ci' / '--configuration-id'.\n"
+
+    @classmethod
+    def get_command_results_upload_with_long_arguments_without_results_argument(cls):
+        return ["results", "upload", "--url", cls.__URL, "--token", cls.__TOKEN, "--testrun-id", cls.__UUID, "--configuration-id", cls.__UUID]
+
+    @classmethod
+    def get_command_results_upload_with_short_arguments_without_results_argument(cls):
+        return ["results", "upload", "-u", cls.__URL, "-t", cls.__TOKEN, "-ti", cls.__UUID, "-ci", cls.__UUID]
+
+    @classmethod
+    def get_output_for_results_upload_without_results_argument(cls):
+        return f"results upload --help' for help.\n\nError: Missing option '-r' / '--results'.\n"
+
+    @classmethod
+    def get_command_testrun_create_with_long_arguments_without_url_argument(cls):
+        return ["testrun", "create", "--token", cls.__TOKEN, "--project-id", cls.__UUID, "--output", cls.__FILE_PATH]
+
+    @classmethod
+    def get_command_testrun_create_with_short_arguments_without_url_argument(cls):
+        return ["testrun", "create", "-t", cls.__TOKEN, "-pi", cls.__UUID, "-o", cls.__FILE_PATH]
+
+    @classmethod
+    def get_output_for_testrun_create_without_url_argument(cls):
+        return f"testrun create --help' for help.\n\nError: Missing option '-u' / '--url'.\n"
+
+    @classmethod
+    def get_command_testrun_create_with_long_arguments_without_token_argument(cls):
+        return ["testrun", "create", "--url", cls.__URL, "--project-id", cls.__UUID, "--output", cls.__FILE_PATH]
+
+    @classmethod
+    def get_command_testrun_create_with_short_arguments_without_token_argument(cls):
+        return ["testrun", "create", "-u", cls.__URL, "-pi", cls.__UUID, "-o", cls.__FILE_PATH]
+
+    @classmethod
+    def get_output_for_testrun_create_without_token_argument(cls):
+        return f"testrun create --help' for help.\n\nError: Missing option '-t' / '--token'.\n"
+
+    @classmethod
+    def get_command_testrun_create_with_long_arguments_without_project_id_argument(cls):
+        return ["testrun", "create", "--url", cls.__URL, "--token", cls.__TOKEN, "--output", cls.__FILE_PATH]
+
+    @classmethod
+    def get_command_testrun_create_with_short_arguments_without_project_id_argument(cls):
+        return ["testrun", "create", "-u", cls.__URL, "-t", cls.__TOKEN, "-o", cls.__FILE_PATH]
+
+    @classmethod
+    def get_output_for_testrun_create_without_project_id_argument(cls):
+        return f"testrun create --help' for help.\n\nError: Missing option '-pi' / '--project-id'.\n"
+
+    @classmethod
+    def get_command_testrun_create_with_long_arguments_without_output_argument(cls):
+        return ["testrun", "create", "--url", cls.__URL, "--token", cls.__TOKEN, "--project-id", cls.__UUID]
+
+    @classmethod
+    def get_command_testrun_create_with_short_arguments_without_output_argument(cls):
+        return ["testrun", "create", "-u", cls.__URL, "-t", cls.__TOKEN, "-pi", cls.__UUID]
+
+    @classmethod
+    def get_output_for_testrun_create_without_output_argument(cls):
+        return f"testrun create --help' for help.\n\nError: Missing option '-o' / '--output'.\n"
+
+    @classmethod
+    def get_command_testrun_complete_with_long_arguments_without_url_argument(cls):
+        return ["testrun", "complete", "--token", cls.__TOKEN, "--testrun-id", cls.__UUID]
+
+    @classmethod
+    def get_command_testrun_complete_with_short_arguments_without_url_argument(cls):
+        return ["testrun", "complete", "-t", cls.__TOKEN, "-ti", cls.__UUID]
+
+    @classmethod
+    def get_output_for_testrun_complete_without_url_argument(cls):
+        return f"testrun complete --help' for help.\n\nError: Missing option '-u' / '--url'.\n"
+
+    @classmethod
+    def get_command_testrun_complete_with_long_arguments_without_token_argument(cls):
+        return ["testrun", "complete", "--url", cls.__URL, "--testrun-id", cls.__UUID]
+
+    @classmethod
+    def get_command_testrun_complete_with_short_arguments_without_token_argument(cls):
+        return ["testrun", "complete", "-u", cls.__URL, "-ti", cls.__UUID]
+
+    @classmethod
+    def get_output_for_testrun_complete_without_token_argument(cls):
+        return f"testrun complete --help' for help.\n\nError: Missing option '-t' / '--token'.\n"
+
+    @classmethod
+    def get_command_testrun_complete_with_long_arguments_without_testrun_id_argument(cls):
+        return ["testrun", "complete", "--url", cls.__URL, "--token", cls.__TOKEN]
+
+    @classmethod
+    def get_command_testrun_complete_with_short_arguments_without_testrun_id_argument(cls):
+        return ["testrun", "complete", "-u", cls.__URL, "-t", cls.__TOKEN]
+
+    @classmethod
+    def get_output_for_testrun_complete_without_testrun_id_argument(cls):
+        return f"testrun complete --help' for help.\n\nError: Missing option '-ti' / '--testrun-id'.\n"
 
     @classmethod
     def get_command_results_import_with_long_arguments(cls):
--- tests/test_click.py
+++ tests/test_click.py
@@ -46,16 +46,46 @@ def test_run_results_with_another_command(runner):
     assert result.exit_code == 2
 
 
-@pytest.mark.parametrize("commands, input, output", [
-    (["results", "import"], Helper.get_input_for_results_import_without_arguments(), Helper.get_output_for_results_import_without_arguments()),
-    (["results", "upload"], Helper.get_input_for_results_upload_without_arguments(), Helper.get_output_for_results_upload_without_arguments()),
-    (["testrun", "complete"], Helper.get_input_for_testrun_complete_without_arguments(), Helper.get_output_for_testrun_complete_without_arguments()),
-    (["testrun", "create"], Helper.get_input_for_testrun_create_without_arguments(), Helper.get_output_for_testrun_create_without_arguments())])
-def test_run_results_with_command_without_arguments(runner, commands, input, output):
-    result = runner.invoke(execute, commands, input=input)
+@pytest.mark.parametrize("commands, output", [
+    (Helper.get_command_results_import_with_long_arguments_without_url_argument(), Helper.get_output_for_results_import_without_url_argument()),
+    (Helper.get_command_results_import_with_short_arguments_without_url_argument(), Helper.get_output_for_results_import_without_url_argument()),
+    (Helper.get_command_results_import_with_long_arguments_without_token_argument(), Helper.get_output_for_results_import_without_token_argument()),
+    (Helper.get_command_results_import_with_short_arguments_without_token_argument(), Helper.get_output_for_results_import_without_token_argument()),
+    (Helper.get_command_results_import_with_long_arguments_without_project_id_argument(), Helper.get_output_for_results_import_without_project_id_argument()),
+    (Helper.get_command_results_import_with_short_arguments_without_project_id_argument(), Helper.get_output_for_results_import_without_project_id_argument()),
+    (Helper.get_command_results_import_with_long_arguments_without_configuration_id_argument(), Helper.get_output_for_results_import_without_configuration_id_argument()),
+    (Helper.get_command_results_import_with_short_arguments_without_configuration_id_argument(), Helper.get_output_for_results_import_without_configuration_id_argument()),
+    (Helper.get_command_results_import_with_long_arguments_without_results_argument(), Helper.get_output_for_results_import_without_results_argument()),
+    (Helper.get_command_results_import_with_short_arguments_without_results_argument(), Helper.get_output_for_results_import_without_results_argument()),
+    (Helper.get_command_results_upload_with_long_arguments_without_url_argument(), Helper.get_output_for_results_upload_without_url_argument()),
+    (Helper.get_command_results_upload_with_short_arguments_without_url_argument(), Helper.get_output_for_results_upload_without_url_argument()),
+    (Helper.get_command_results_upload_with_long_arguments_without_token_argument(), Helper.get_output_for_results_upload_without_token_argument()),
+    (Helper.get_command_results_upload_with_short_arguments_without_token_argument(), Helper.get_output_for_results_upload_without_token_argument()),
+    (Helper.get_command_results_upload_with_long_arguments_without_testrun_id_argument(), Helper.get_output_for_results_upload_without_testrun_id_argument()),
+    (Helper.get_command_results_upload_with_short_arguments_without_testrun_id_argument(), Helper.get_output_for_results_upload_without_testrun_id_argument()),
+    (Helper.get_command_results_upload_with_long_arguments_without_configuration_id_argument(), Helper.get_output_for_results_upload_without_configuration_id_argument()),
+    (Helper.get_command_results_upload_with_short_arguments_without_configuration_id_argument(), Helper.get_output_for_results_upload_without_configuration_id_argument()),
+    (Helper.get_command_results_upload_with_long_arguments_without_results_argument(), Helper.get_output_for_results_upload_without_results_argument()),
+    (Helper.get_command_results_upload_with_short_arguments_without_results_argument(), Helper.get_output_for_results_upload_without_results_argument()),
+    (Helper.get_command_testrun_create_with_long_arguments_without_url_argument(), Helper.get_output_for_testrun_create_without_url_argument()),
+    (Helper.get_command_testrun_create_with_short_arguments_without_url_argument(), Helper.get_output_for_testrun_create_without_url_argument()),
+    (Helper.get_command_testrun_create_with_long_arguments_without_token_argument(), Helper.get_output_for_testrun_create_without_token_argument()),
+    (Helper.get_command_testrun_create_with_short_arguments_without_token_argument(), Helper.get_output_for_testrun_create_without_token_argument()),
+    (Helper.get_command_testrun_create_with_long_arguments_without_project_id_argument(), Helper.get_output_for_testrun_create_without_project_id_argument()),
+    (Helper.get_command_testrun_create_with_short_arguments_without_project_id_argument(), Helper.get_output_for_testrun_create_without_project_id_argument()),
+    (Helper.get_command_testrun_create_with_long_arguments_without_output_argument(), Helper.get_output_for_testrun_create_without_output_argument()),
+    (Helper.get_command_testrun_create_with_short_arguments_without_output_argument(), Helper.get_output_for_testrun_create_without_output_argument()),
+    (Helper.get_command_testrun_complete_with_long_arguments_without_url_argument(), Helper.get_output_for_testrun_complete_without_url_argument()),
+    (Helper.get_command_testrun_complete_with_short_arguments_without_url_argument(), Helper.get_output_for_testrun_complete_without_url_argument()),
+    (Helper.get_command_testrun_complete_with_long_arguments_without_token_argument(), Helper.get_output_for_testrun_complete_without_token_argument()),
+    (Helper.get_command_testrun_complete_with_short_arguments_without_token_argument(), Helper.get_output_for_testrun_complete_without_token_argument()),
+    (Helper.get_command_testrun_complete_with_long_arguments_without_testrun_id_argument(), Helper.get_output_for_testrun_complete_without_testrun_id_argument()),
+    (Helper.get_command_testrun_complete_with_short_arguments_without_testrun_id_argument(), Helper.get_output_for_testrun_complete_without_testrun_id_argument())])
+def test_run_results_with_command_without_arguments(runner, commands, output):
+    result = runner.invoke(execute, commands)
 
     assert output in result.output
-    assert result.exit_code == 1
+    assert result.exit_code == 2
 
 
 @pytest.mark.parametrize("commands_with_args", [
