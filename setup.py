08c690d3eb2737b21c4131c0c38799325fdb6e21
pavel.butuzov <pavel.butuzov@testit.software>
2025-03-27 11:02:21 +0300

    feat: update the api-client version.

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import find_packages, setup
 
-VERSION = "2.4.0"
+VERSION = "2.4.1"
 
 setup(
     name='testit-cli',
@@ -26,7 +26,7 @@ setup(
     py_modules=['testit_cli'],
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
-    install_requires=['testit-api-client==5.3.0', 'validators', 'tqdm', 'click~=8.0.4'],
+    install_requires=['testit-api-client==6.0.0', 'validators', 'tqdm', 'click~=8.0.4'],
     entry_points={
         'console_scripts': [
             'testit = testit_cli.__main__:console_main'
