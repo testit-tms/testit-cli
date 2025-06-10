25dd58c75447667c5e6412a5b5fb20aa665bce80
pavel.butuzov <pavel.butuzov@testit.software>
2025-06-10 10:05:12 +0300

    feat: support api-client 6.1.

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import find_packages, setup
 
-VERSION = "2.4.7.post530"
+VERSION = "2.4.8"
 
 setup(
     name='testit-cli',
@@ -26,7 +26,7 @@ setup(
     py_modules=['testit_cli'],
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
-    install_requires=['testit-api-client==6.0.1.post530', 'validators', 'tqdm', 'click~=8.0.4'],
+    install_requires=['testit-api-client==6.1.0', 'validators', 'tqdm', 'click~=8.0.4'],
     entry_points={
         'console_scripts': [
             'testit = testit_cli.__main__:console_main'
